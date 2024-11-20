9895902fc840fc786fb18bdc4453500eec473688
Antonio Soriano <fotosportafolio2@gmail.com>
2024-11-20 14:36:43 -0600

    Delete examen directory

--- examen/botones/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll("button").forEach(button => {
-    button.addEventListener("click", function() {
-        this.style.backgroundColor = "red";
-        this.style.color = "black";
-    });
-});
